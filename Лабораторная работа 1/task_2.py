50571e15c9b1ec8db36db610061f2533e581e4cf
Ferzalieva <148002816+Ferzalieva@users.noreply.github.com>
2023-10-15 20:57:03 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_2.py
@@ -0,0 +1,9 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+len(list_players)
+one_list = list_players[0:3]  # Первая команда
+two_list = list_players[3:6]  # Вторая команда
+print(one_list)
+print(two_list)
